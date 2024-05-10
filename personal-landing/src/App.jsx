4b0fc3607d1ef6dcf09cf6d292793fa42c52a3eb
Valeriy Goncharov <solairw.coding@gmail.com>
2024-05-10 12:37:19 +1000

    Initial commit

---
+++ personal-landing/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "@fontsource/cascadia-code"
+
+function App() {
+  return (
+    <>
+      <div id="main-view">
+        <h1 id="name">Валерий Гончаров</h1>
+        Разработчик C#, Владивосток
+      </div>
+    </>
+  )
+}
+
+export default App
